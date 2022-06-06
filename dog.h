ab315c6c8c33acc055f08cf834666cd73d96639d
beksultan10 <94545033+beksultan10@users.noreply.github.com>
2022-06-06 21:29:47 +0600

    Update dog.h

--- dog.h
+++ dog.h
@@ -16,3 +16,31 @@ public:
 	void Show() override;
 	void Type() override;
 };
+
+
+dog.cpp
+#include "dog.h"
+
+dog::dog()
+{
+}
+
+dog::dog(string tp, string nm, size_t ag, string hng) : pet (nm, ag, hng)
+{
+	type = tp;
+}
+
+void dog::Sound()
+{
+	cout << "gav gav" << endl;
+}
+
+void dog::Show()
+{
+	cout << getname << endl;
+}
+
+void dog::Type()
+{
+
+}
